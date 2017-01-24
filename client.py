ee75b3ee1e28466c3a5a4abdff7d78084ee53bb3
capalmer1013 <capalmer1013@gmail.com>
2017-01-23 22:46:40 -0500

    stop the server from hanging up the port... hopefully

--- client.py
+++ client.py
@@ -1,11 +1,15 @@
 # Echo client program
 import socket
 
-HOST = 'localhost'    # The remote host
+HOST = '54.218.251.198'    # The remote host
 PORT = 50007              # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
-data = s.recv(1024)
-s.close()
+try:
+    while True:
+        newMessage = raw_input(">>>")
+        s.sendall(">>>"+str(newMessage))
+        data = s.recv(1024)
+except KeyboardInterrupt:
+    s.close()
 print 'Received', repr(data)
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 # Echo server program
 import socket
+import threading
 
 HOST = ''                 # Symbolic name meaning all available interfaces
 PORT = 50007              # Arbitrary non-privileged port
@@ -7,6 +8,14 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 conn, addr = s.accept()
+
+class listenForConnectionsThread(threading.Thread):
+    def __init__(self):
+        self.connectionList = []
+        pass
+
+    def run(self):
+        pass
 print 'Connected by', addr
 while 1:
     data = conn.recv(1024)
